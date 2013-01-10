70920b786218120a0989d5431ca1fa9f330aa52f
Jesus Fernandez <jesus@playhaven.com>
2013-01-09 17:01:18 -0800

    PHTimeInGame documentation

--- src/PHTimeInGame.h
+++ src/PHTimeInGame.h
@@ -8,23 +8,33 @@
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
-    
+
+//  Singleton class that keeps track of elapsed session count and session time.
+//  Stores data in NSUserDefaults until the next successful open request.
+//  Information collected by this class is used for time-in-game segmentation
+//  and targeting.
 @interface PHTimeInGame : NSObject {
 
     CFAbsoluteTime sessionStartTime;
 }
 
+//  Singleton accessor
 +(PHTimeInGame *) getInstance;
 
+//  Start counting up time for the current session
 -(void) gameSessionStarted;
+//  Ends  the current session and increments counters
 -(void) gameSessionStopped;
+//  Resets time and session data. Typically used after data is sent to the API.
+-(void) resetCounters;
+//  Re-initializes session and time data. Only used for unit tests
 -(void) gameSessionRestart;
 
--(void) resetCounters;
-
-// These should only be called with-in the PH SDK
+//  Returns total unreported session duration, in seconds
 -(CFAbsoluteTime) getSumSessionDuration;
+//  Returns total unreported sessions
 -(int) getCountSessions;
+//  Returns current session duration, in seconds
 -(CFAbsoluteTime) getCurrentSessionDuration;
 
 @end
